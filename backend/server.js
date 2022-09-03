40ed31aea387be42a80f7c442d17202244406e4a
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-03 21:55:30 +0800

    Add elementary exception middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware.js')
 const port = process.env.PORT 
 
 const app = express()
@@ -8,5 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/tasks', require('./routes/taskRoutes.js'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
