f3bb4fae7d205cdb2d6c4be98a513a7aa1a1f441
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-04 12:52:55 +0800

    Add register cleaner functionality

--- backend/controllers/cleanerController.js
+++ backend/controllers/cleanerController.js
@@ -1,9 +1,49 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const Cleaner = require('../models/cleanerModel.js')
+
 // @desc    Create a cleaner
 // @route   POST /api/cleaners
 // @access  ?
-const registerCleaner = (req, res) => {
-    res.json({ message: 'register user'} )
-}
+const registerCleaner = asyncHandler(async (req, res) => {
+    const { name, email, password, contract_start, contract_end, role } = req.body
+
+    if ( !name || !email || !password || !contract_start || !contract_end || !role ){
+        res.status(400)
+        throw new Error('Missing required fields.')
+    }
+
+    const cleanerExists = await Cleaner.findOne({email})
+    if (cleanerExists){
+        res.status(400)
+        throw new Error('Cleaner already exists.')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPass = await bcrypt.hash(password, salt)
+
+    const cleaner = await Cleaner.create({
+        name,
+        email,
+        password: hashedPass,
+        contract_start,
+        contract_end,
+        role
+    })
+
+    if (cleaner){
+        // res.status(201).json({
+        //     _id: cleaner.id,
+        //     name: cleaner.name,
+        //     email: cleaner.email
+        // })
+        res.status(201).json({ message: `Account for cleaner ${name} had been created.`})
+    } else {
+        res.status(400)
+        throw new Error('Unable to create an account.')
+    }
+})
 
 // @desc    Log in a cleaner
 // @route   POST /api/cleaners/login
