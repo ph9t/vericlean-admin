75716af762ae1adc39fdc2039e9663a3e6b6030f
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-07 14:34:55 +0800

    Add model for 'Head Household' collection

---
+++ backend/models/headModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const headSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Missing name.'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Missing email.'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Missing password.']
+    },
+    role: {
+        type: String,
+        required: [true, 'Missing role.']
+    }
+}, {
+    timestamp: true
+})
+
+module.exports = mongoose.model('Head', headSchema)
