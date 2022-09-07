61cebb4cfb663a8d7ec69d16a9003f0ab49f22bd
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-07 16:05:41 +0800

    Add route protection for 'scheduled task' resource

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -6,8 +6,10 @@ const {
     updateTask,
     deleteTask
 } = require('../controllers/taskController.js')
+const { protect } = require('../middleware/authMiddleware.js')
+const { adminProtect } = require('../middleware/headAuthMiddleware.js')
 
-router.route('/').get(getTasks).post(setTask)
-router.route('/:id').put(updateTask).delete(deleteTask)
+router.route('/').get(protect, getTasks).post(adminProtect, setTask)
+router.route('/:id').put(adminProtect, updateTask).delete(adminProtect, deleteTask)
 
 module.exports = router
