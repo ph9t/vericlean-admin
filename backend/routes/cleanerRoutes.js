37eca2115ec3f7b41864680dff8d24e8311f50f2
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-07 15:56:24 +0800

    Add new routes for 'cleaner' resource

--- backend/routes/cleanerRoutes.js
+++ backend/routes/cleanerRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerCleaner, loginCleaner, getMe } = require('../controllers/cleanerController.js')
+const { registerCleaner, loginCleaner, allCleaners, getMe } = require('../controllers/cleanerController.js')
 const { protect } = require('../middleware/authMiddleware.js')
 const { adminProtect } = require('../middleware/headAuthMiddleware.js')
 
 router.post('/', adminProtect, registerCleaner)
 router.post('/login', loginCleaner)
+router.get('all', adminProtect, allCleaners)
 router.get('/me', protect, getMe) // tentative
 
 module.exports = router
