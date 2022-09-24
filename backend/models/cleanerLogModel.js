c815dc1b9e2302ed225b07bd0a1bd580ffe6e89d
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-25 03:57:47 +0800

    Add model for Cleaner Log collection

---
+++ backend/models/cleanerLogModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const cleanerLogSchema = mongoose.Schema({
+    cleaner_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Cleaner'
+    },
+    scheduled_task_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Task'
+    },
+    video_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Video'
+    },
+    qr_log_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'QuickR'
+    },
+},
+{
+    timestamp: true
+})
+
+module.exports = mongoose.model('CleanerLog', cleanerLogSchema)
