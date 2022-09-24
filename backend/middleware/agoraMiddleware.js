e2876c6457380abfc32247ee275e5593e584b4b4
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-25 03:33:23 +0800

    Add middleware for Agora RTC web token generator controller

---
+++ backend/middleware/agoraMiddleware.js
@@ -0,0 +1,11 @@
+const nocache = (_, resp, next) => {
+    resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+    resp.header('Expires', '-1')
+    resp.header('Pragma', 'no-cache')
+
+    next()
+}
+
+module.exports = {
+    nocache
+}
