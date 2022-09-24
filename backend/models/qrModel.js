51eb36de7d6115d0be8e4234563f960bd14a1c01
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-25 03:55:29 +0800

    Add model for QR Log collection

---
+++ backend/models/qrModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const qrSchema = mongoose.Schema({
+    cleaner_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Cleaner'
+    },
+    scan_time_in: {
+        type: Date,
+    },
+    scan_time_out: {
+        type: Date,
+    },
+}, {
+    timestamp: true
+})
+
+module.exports = mongoose.model('QuickR', qrSchema)
