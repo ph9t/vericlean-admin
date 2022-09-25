38ce185db497af9560971678343ecf549d5b0e08
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-25 10:29:34 +0800

    Add POST endpoint for HH resource

--- backend/routes/headRoutes.js
+++ backend/routes/headRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { loginHead, headMe } = require('../controllers/headController.js')
+const { loginHead, registerHead } = require('../controllers/headController.js')
 
 router.post('/login', loginHead)
+router.post('/register', registerHead)
 
 module.exports = router
