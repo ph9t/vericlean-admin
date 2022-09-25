97456528d31bb5645c39c712b374e1a5dd0dcf9f
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-25 10:23:19 +0800

    Add 'registerHead' controller logic for 'Head Household' resource

--- backend/controllers/headController.js
+++ backend/controllers/headController.js
@@ -1,30 +1,46 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const asyncHandler = require('express-async-handler')
-const Head = require('../models/headModel.js')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const Head = require("../models/headModel.js");
 
-const loginHead = asyncHandler( async (req, res) => {
-    const { email, password } = req.body
-    const head = await Head.findOne({ email })
+// @desc    Register Head Household
+// @route   POST /api/heads/register
+// @access  Public
+const registerHead = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
 
-    if (head && (await bcrypt.compare(password, head.password))){
-        const payload = { _id: head._id, role: head.role }
-        res.json({
-            _id: head.id,
-            name: head.name,
-            email: head.email,
-            token: jwt.sign(payload, process.env.JWT_SECRET, {
-                expiresIn: '30d'
-            })
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid credentials for a Head Household.')
-    }
-})
+  if (!name || !email || !password || !role) {
+    res.status(400);
+    throw new Error("Missing required fields.");
+  }
 
-const headMe = asyncHandler(async (req, res) => {
+  const headExists = await Head.findOne({ email });
 
-})
+  if (headExists) {
+    res.status(400);
+    throw new Error("Head Household already exists.");
+  }
 
-module.exports = { loginHead, headMe }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const head = await Head.create({
+    name,
+    email,
+    password: hashedPass,
+    role,
+  });
+
+  if (head) {
+    res.status(201).json({
+      _id: head.id,
+      name: head.name,
+      email: head.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Unable to create an account.");
+  }
+});
+
+module.exports = { loginHead, registerHead, headMe };
