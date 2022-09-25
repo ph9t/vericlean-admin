a7d91b3cabcb7a80508ade981b3405feae09b8c7
Angel Jimenez <abruce.jimenez@gmail.com>
2022-09-25 10:26:56 +0800

    Add 'loginHead' controller logic for 'Head Household' resource

--- backend/controllers/headController.js
+++ backend/controllers/headController.js
@@ -43,4 +43,27 @@ const registerHead = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Login Head Household
+// @route   POST /api/heads/login
+// @access  Public
+const loginHead = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const head = await Head.findOne({ email });
+
+  if (head && (await bcrypt.compare(password, head.password))) {
+    const payload = { _id: head._id, role: head.role };
+    res.json({
+      _id: head.id,
+      name: head.name,
+      email: head.email,
+      token: jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+      }),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials for a Head Household.");
+  }
+});
+
 module.exports = { loginHead, registerHead, headMe };
