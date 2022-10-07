b6f7fcf9891e70d0fdbd54068f4d7e50c04a9260
Angel Jimenez <abruce.jimenez@gmail.com>
2022-10-07 19:39:46 +0800

    Refactor authentication middleware for general user

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,33 +1,45 @@
-const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
-const Cleaner = require('../models/cleanerModel.js')
-const Head = require('../models/headModel.js')
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const Cleaner = require("../models/cleanerModel.js");
+const Head = require("../models/headModel.js");
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token
+  let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try {
-            token = req.headers.authorization.split(' ')[1]
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            if (decoded.role === 'cleaner'){
-                req.cleaner = await Cleaner.findById(decoded._id).select('-password')
-            } else if (decoded.role === 'head'){
-                req.head = await Head.findById(decoded._id).select('-password')
-            }
-            next()
-        } catch (error){
-            console.log(error)
-            res.status(401)
-            throw new Error('User not authorized.')
-        }
+      const cleaner = await Cleaner.findOne({ email: decoded.email }).select(
+        "-password"
+      );
+
+      if (!cleaner) {
+        req.who = "head";
+        req.head = await Head.findOne({ email: decoded.email }).select(
+          "-password"
+        );
+      } else {
+        req.who = "cleaner";
+        req.cleaner = cleaner;
+      }
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("User not authorized.");
     }
+  }
 
-    if (!token) {
-        res.status(401)
-        throw new Error('Not authorized because no token sent in headers.')
-    }
-})
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized because no token sent in headers.");
+  }
+});
 
-module.exports = { protect }
+module.exports = { protect };
