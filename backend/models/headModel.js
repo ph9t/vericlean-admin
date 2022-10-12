165959e83e4e0596d6e631b7966a069f7d08068f
Angel Jimenez <abruce.jimenez@gmail.com>
2022-10-13 03:55:31 +0800

    Update fields in the model of the 'Head Household' collection

--- backend/models/headModel.js
+++ backend/models/headModel.js
@@ -2,22 +2,26 @@ const mongoose = require("mongoose");
 
 const headSchema = mongoose.Schema(
   {
-    name: {
+    first_name: {
       type: String,
-      required: [true, "Missing name."],
+      required: [true, "Mising field: first name."]
+    },
+    last_name: {
+      type: String,
+      required: [true, "Missing field: last name."]
     },
     email: {
       type: String,
-      required: [true, "Missing email."],
+      required: [true, "Missing field: email."],
       unique: true,
     },
     password: {
       type: String,
-      required: [true, "Missing password."],
+      required: [true, "Missing field: password."],
     },
     role: {
       type: String,
-      required: [true, "Missing role."],
+      required: [true, "Missing field: role."],
     },
   },
   {
