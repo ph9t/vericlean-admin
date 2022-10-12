7612684da94a2736311b043905d39ec04bab8ebf
Angel Jimenez <abruce.jimenez@gmail.com>
2022-10-13 04:16:08 +0800

    Refactor controller logic for 'QR Log' collection

--- backend/controllers/qrController.js
+++ backend/controllers/qrController.js
@@ -3,11 +3,6 @@ const QuickR = require("../models/qrModel.js");
 const CleanerLog = require("../models/cleanerLogModel.js");
 
 const scanQr = asyncHandler(async (req, res) => {
-  if (!req.params.task_id) {
-    res.status(400);
-    throw new Error("Route requires 'task_id'.");
-  }
-
   if (!req.body.scan_time_in && !req.body.scan_time_out) {
     res.status(400);
     throw new Error(
@@ -15,20 +10,20 @@ const scanQr = asyncHandler(async (req, res) => {
     );
   }
 
-  const cleanerLog = await CleanerLog.find({
-    scheduled_task_id: req.params.task_id,
+  const cleanerLog = await CleanerLog.findOne({
+    scheduled_task_id: req.params.id,
     cleaner_id: req.cleaner.id,
   });
 
   if (!cleanerLog) {
     res.status(400);
     throw new Error(
-      `Cannot find CleanerLog with 'task_id' of ${req.params.task_id} and 'cleaner_id' of ${req.cleaner._id}`
+      `Cannot find CleanerLog with task 'id' of ${req.params.id} and 'cleaner_id' of ${req.cleaner._id}`
     );
   }
 
   const quickR = await QuickR.findByIdAndUpdate(
-    cleanerLog[0].qr_log_id,
+    cleanerLog.qr_log_id,
     req.body,
     {
       new: true,
@@ -39,24 +34,20 @@ const scanQr = asyncHandler(async (req, res) => {
 });
 
 const getQr = asyncHandler(async (req, res) => {
-  if (!req.params.task_id) {
-    res.status(400);
-    throw new Error("Route requires 'task_id'.");
-  }
 
-  const cleanerLog = await CleanerLog.find({
-    scheduled_task_id: req.params.task_id,
+  const cleanerLog = await CleanerLog.findOne({
+    scheduled_task_id: req.params.id,
     cleaner_id: req.cleaner.id,
   });
 
   if (!cleanerLog) {
     res.status(400);
     throw new Error(
-      `Cannot find CleanerLog with 'task_id' of ${req.params.task_id} and 'cleaner_id' of ${req.cleaner._id}`
+      `Cannot find CleanerLog with task 'id' of ${req.params.id} and 'cleaner_id' of ${req.cleaner._id}`
     );
   }
 
-  const quickR = await QuickR.findById(cleanerLog[0].qr_log_id);
+  const quickR = await QuickR.findById(cleanerLog.qr_log_id);
   res.status(200).json(quickR);
 });
 
