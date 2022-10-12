839bbaf6117e8bebfe40103b98faf8f6d07ad4c5
Angel Jimenez <abruce.jimenez@gmail.com>
2022-10-13 04:17:29 +0800

    Change parameter name of a route for the 'QR Log' collection

--- backend/routes/qrRoutes.js
+++ backend/routes/qrRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { cleanerProtect } = require('../middleware/cleanerAuthMiddleware.js')
+const { boundTask } = require('../middleware/taskTimeBoundMiddleware.js')
 const { scanQr, getQr } = require('../controllers/qrController.js')
 
-router.route('/:task_id').get(cleanerProtect, getQr).put(cleanerProtect, scanQr)
+router.route('/:id').get(cleanerProtect, getQr).put(cleanerProtect, boundTask, scanQr)
 
 module.exports = router
