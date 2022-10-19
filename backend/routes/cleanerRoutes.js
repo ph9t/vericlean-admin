dabb1a389b9e2f95d78f11a4892eb9c3e0e56173
Angel Jimenez <abruce.jimenez@gmail.com>
2022-10-19 18:35:45 +0800

    Update route file for 'Cleaner' endpoints

--- backend/routes/cleanerRoutes.js
+++ backend/routes/cleanerRoutes.js
@@ -1,13 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { registerCleaner, loginCleaner, allCleaners, getMe } = require('../controllers/cleanerController.js')
-const { protect } = require('../middleware/authMiddleware.js')
-const { headProtect } = require('../middleware/headAuthMiddleware.js')
+const {
+  registerCleaner,
+  loginCleaner,
+  allCleaners,
+  getStats,
+  getDetails,
+  updateCleaner,
+  deleteCleaner,
+} = require("../controllers/cleanerController.js");
+// const { protect } = require("../middleware/authMiddleware.js");
+const { headProtect } = require("../middleware/headAuthMiddleware.js");
+const { cleanerProtect } = require("../middleware/cleanerAuthMiddleware.js");
+const { protect } = require("../middleware/authMiddleware.js");
 
-router.post('/', headProtect, registerCleaner)
-router.post('/login', loginCleaner)
-router.get('/all', headProtect, allCleaners)
-router.get('/me', protect, getMe) // tentative
+router.post("/", headProtect, registerCleaner);
+router.post("/login", loginCleaner);
+router.get("/all", headProtect, allCleaners);
+router.get("/stats", headProtect, getStats);
+router.get("/me", cleanerProtect, getDetails); // tentative
+router
+  .route("/:id")
+  .put(protect, updateCleaner)
+  .delete(headProtect, deleteCleaner);
 
-module.exports = router
+module.exports = router;
