dcde54041c70d6b7f2e3e0000ff4277d50dc2f0d
Angel Jimenez <abruce.jimenez@gmail.com>
2022-10-19 18:44:57 +0800

    Update route file for Scheduled Task endpoints

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const { 
-    getTasks,
-    setTask,
-    updateTask,
-    deleteTask
-} = require('../controllers/taskController.js')
-const { protect } = require('../middleware/authMiddleware.js')
-const { headProtect } = require('../middleware/headAuthMiddleware.js')
+const express = require("express");
+const router = express.Router();
+const {
+  getStats,
+  getTasks,
+  setTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController.js");
+const { protect } = require("../middleware/authMiddleware.js");
+const { headProtect } = require("../middleware/headAuthMiddleware.js");
 
-router.route('/').get(protect, getTasks).post(headProtect, setTask)
-router.route('/:id').put(headProtect, updateTask).delete(headProtect, deleteTask)
+router.route("/").get(protect, getTasks).post(headProtect, setTask);
+router.route("/stats").get(headProtect, getStats)
+router
+  .route("/:id")
+  .put(headProtect, updateTask)
+  .delete(headProtect, deleteTask);
 
-module.exports = router
+module.exports = router;
